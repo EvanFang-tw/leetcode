2bad0edb7e8b950b6803822ab9d02cfe9932c1dc
Evan Fang <evanfang.hi@gmail.com>
2021-01-19 14:15:57 +0800

    Add second solution

--- sql/181.sql
+++ sql/181.sql
@@ -1,3 +1,4 @@
+-- Solution 1
 select e3.Name as Employee from (
   select
     Name,
@@ -6,3 +7,11 @@ select e3.Name as Employee from (
     from Employee as e1
   ) as e3
 where e3.Salary > ManagerSalary;
+
+-- Solution 2
+select tt.Employee from (
+	select e1.Name as Employee, e1.Salary, e2.Salary as ManagerSalary from Employee as e1
+	left join Employee as e2
+	on e1.ManagerId = e2.id
+) as tt
+where tt.Salary > tt.ManagerSalary;
